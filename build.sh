9fed1bb9f6741f5b3c080460ce0468c028af03af
Flopster101 <nahuelgomez329@gmail.com>
2025-11-15 13:12:44 -0300

    build: Don't delete out directory
    Why?
    
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- build.sh
+++ build.sh
@@ -42,7 +42,4 @@ echo "Done copying modules into modulebuild."
 cp -nf $(pwd)/kernelbuild/Image $(pwd)/AnyKernel3
 cp -nr $(pwd)/modulebuild/*.ko $(pwd)/AnyKernel3/modules/system/lib/modules
 cd AnyKernel3 && zip -r9 UPDATE-AnyKernel3-gta9.zip * -x .git README.md *placeholder
-# Cleanups
-echo "Cleaning out/ directory..."
-rm -rf out/
 echo "Done."
