caf7dead2c11161278b3df03f51437933789977a
techyguyperplexable <hlcj1234567@gmail.com>
2025-12-07 13:18:14 +0000

    qcacld-3.0: Stub missing ucfg_reg_cache_channel_state
    Change-Id: Id78aba278e74f9de5da18e6805dc03b8cb55d7dc
    Signed-off-by: techyguyperplexable <hlcj1234567@gmail.com>

--- drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_ioctl.c
+++ drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_ioctl.c
@@ -1,3 +1,4 @@
+#define ucfg_reg_cache_channel_state(...) do { } while (0)
 /*
  * Copyright (c) 2012-2021 The Linux Foundation. All rights reserved.
  * Copyright (c) 2021-2023 Qualcomm Innovation Center, Inc. All rights reserved.
